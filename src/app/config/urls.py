ef0c1074a8fe09c42f26a94da03f72a549ef9ae9
capitanx9 <capitanx9@gmail.com>
2026-04-29 14:50:28 +0200

    feat: auth with signup/login + password reset + GitHub OAuth (#15)

--- src/app/config/urls.py
+++ src/app/config/urls.py
@@ -4,4 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("health/", include("app.core.urls")),
+    path("accounts/", include("allauth.urls")),
+    path("", include("app.users.urls")),
 ]
